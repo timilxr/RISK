c7175d11945f67f247361924ad4f357456ccb3d7
AYORINDE TIMILEHIN <ayorindeoluwatimilehin@gmail.com>
2018-05-30 23:23:26 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path: '', component: LandingComponent},
+      {path: 'home', component: LandingComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+  // number(){
+  //   alert("this is me");
+  // }
+  // others(){
+  //   alert("<input type='text' class='form-control' placeholder='other?'>");
+  //   }
+  //   yesnoCheck(){
+  //     if ( document.getElementById("me").click){
+  //       document.getElementById("ifYes").style.display = "block";
+  //     } else {
+  //       document.getElementById("ifYes").style.display = "none";
+  //     }
+  //   }
+
+  ngOnInit() {
+  }
+
+}
