b239a05d6122fc61963534b377fddc9a7aa033d9
AYORINDE TIMILEHIN <ayorindeoluwatimilehin@gmail.com>
2018-08-02 17:44:51 +0100

    third updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/treatrisk'));
+app.use(express.static(__dirname + '/dist/RISK'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/treatrisk/index.html'));
+res.sendFile(path.join(__dirname+'/dist/RISK/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
