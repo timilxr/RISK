e664bb9573c027698e9ee48752b3edd8650a493d
AYORINDE TIMILEHIN <ayorindeoluwatimilehin@gmail.com>
2018-08-07 12:04:30 +0100

    updated on 7-08-18

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/RISK'));
+app.use(express.static(__dirname + '/dist/risk'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/RISK/index.html'));
+res.sendFile(path.join(__dirname+'/dist/risk/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
